119c1227e319213bf538b7a1939ac8ab01a5b109
promiseowolabi <promise.owolabi@me.com>
2019-07-04 14:58:24 +0100

    upates to vmware live mount

--- python/vmware_live_mount.py
+++ python/vmware_live_mount.py
@@ -3,9 +3,9 @@ import urllib3
 urllib3.disable_warnings()
 
 node_ip = '10.10.10.10'
-username = 'demo@rubrikdemo.com'
-password = 'password'
-vm_name = "em1-promowol-l1"
+username = 'test@rubrikdemo.com'
+password = 'test'
+vm_name = "vm-name"
 
 rubrik = rubrik_cdm.Connect(node_ip, username, password, enable_logging=True)
 
@@ -14,4 +14,4 @@ print(cluster_version)
 
 #instant_recovery = rubrik.vsphere_instant_recovery(vm_name, date, time)
 
-live_mount = rubrik.vsphere_live_mount(vm_name, date='06-24-2018', time='12:00 AM')
+live_mount = rubrik.vsphere_live_mount(vm_name, date='07-02-2019', time='03:03 PM')
