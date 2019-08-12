2f3aa12b8c43b749b108062fe35a54c86fdfe58c
promiseowolabi <promise.owolabi@me.com>
2019-08-12 16:04:39 +0100

    added gets mount details

--- python/get_vmware_live_mount_name.py
+++ python/get_vmware_live_mount_name.py
@@ -2,10 +2,10 @@ import rubrik_cdm
 import urllib3
 urllib3.disable_warnings()
 
-vm_name = "em1-promowol-l1"
+vm_name = "em2-nikgrove-l1"
 
 rubrik = rubrik_cdm.Connect(enable_logging=True)
 
-live_mount = rubrik.vsphere_live_mount(vm_name)
+live_mount = rubrik.get_vsphere_live_mount_names(vm_name)
 
 print(live_mount)
