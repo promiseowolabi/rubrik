4b344549d80cce779ef5cc130aa6a27d7bd2502f
promiseowolabi <promise.owolabi@me.com>
2019-08-27 11:46:07 +0100

    added sql instant recovery

--- python/sql_instant_recovery.py
+++ python/sql_instant_recovery.py
@@ -6,10 +6,11 @@ urllib3.disable_warnings()
 db_name = "AdventureWorks2016"
 sql_instance = 'MSSQLSERVER'
 sql_host = 'em1-promowol-w1.rubrikdemo.com'
-mount_name = 'AdventureClone'
 date = '08-20-2019'
 time = '08:38 AM'
 
 rubrik = rubrik_cdm.Connect(enable_logging=True)
 
-sql_live_mount = rubrik.sql_live_mount(db_name, date, time, sql_instance, sql_host, mount_name)
+sql_instant_recovery = rubrik.sql_instant_recovery(db_name, date, time, sql_instance, sql_host)
+
+print(sql_instant_recovery)
