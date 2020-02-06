f1bc337d5f8227671d3aa0dbaab36143220cc4ff
promiseowolabi <promise.owolabi@me.com>
2020-02-06 23:13:09 +0000

    updates sql live mount with latest

--- python/mssql/sql_live_mount.py
+++ python/mssql/sql_live_mount.py
@@ -4,12 +4,14 @@ import urllib3
 urllib3.disable_warnings()
 
 db_name = "AdventureWorks2016"
+#db_name = 'chris'
 sql_instance = 'MSSQLSERVER'
 sql_host = 'em1-promowol-w1.rubrikdemo.com'
-mount_name = 'AdventureClone'
-date = '08-20-2019'
-time = '08:38 AM'
+#sql_host = 'EM1-chrileco-w1'
+mount_name = 'AdventureClone_log'
+date = '02-06-2020'
+time = '08:00 AM'
 
 rubrik = rubrik_cdm.Connect(enable_logging=True)
 
-sql_live_mount = rubrik.sql_live_mount(db_name, date, time, sql_instance, sql_host, mount_name)
+sql_live_mount = rubrik.sql_live_mount(db_name, sql_instance, sql_host, mount_name)
