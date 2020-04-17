0bb4d77d067d532b8b89bac3576f4dd7c62ff972
Promise Owolabi <promise.owolabi@me.com>
2020-04-17 14:18:20 +0100

    variable errors

--- python/vmware/get_rubrik_sla.py
+++ python/vmware/get_rubrik_sla.py
@@ -5,7 +5,7 @@ urllib3.disable_warnings()
 
 rubrik = rubrik_cdm.Connect(enable_logging=True)
 
-get_sla = rubrik.get()
-
-for item in get_vsphere_vm['data']:
-    print(f"{item['name']} SLA is {item['slaAssignment']}")
+get_sla = rubrik.get_vsphere_vm()
+for item in get_sla['data']:
+    print(f"{item['name']} SLA is {item['effectiveSlaDomainName']}")
+          
